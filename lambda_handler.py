d6de055d93b71bfea3b00aa441a7db3a5cfb622c
davnm5 <josdanei@espol.edu.ec>
2022-08-20 12:50:03 -0500

    DEPLOY

--- lambda_handler.py
+++ lambda_handler.py
@@ -12,6 +12,17 @@ def suma(event, context):
         "suma": operando_b + operando_a
     }
 
+def resta(event, context):
+    operando_a = event['operando_a']
+    operando_b = event['operando_b']
+    if not isinstance(operando_a, int) or not isinstance(operando_b, int):
+        raise Exception('Parametros no son numeros enteros')
+    return {
+        "status": 200,
+        "message": "ok",
+        "suma": operando_b - operando_a
+    }
+
 
 def fecha_actual(event, context):
     return {
